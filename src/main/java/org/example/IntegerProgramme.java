f770b593d3dd2f89123b48c3db353e5fcf199e8b
fcec4 <fcec47@gmail.com>
2024-05-24 17:17:28 +0530

    IntegerProgramme

---
+++ src/main/java/org/example/IntegerProgramme.java
@@ -0,0 +1,10 @@
+package org.example;
+import java.util.Scanner;
+public class IntegerProgramme {
+    public static void main(String[] args) {
+        int x ;
+        Scanner s = new Scanner(System.in);
+        x = s.nextInt() ;
+        System.out.println(x);
+    }
+}
